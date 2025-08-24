6b65a92e8fe76092dacf8bb6cfc0f95543e672bd
Ritesh Sharma <riteshrsharma71@gmail.com>
2025-08-24 15:19:00 +0530

    updated redirection to loging for /

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,15 +2,16 @@
 import Home from "./Components/Home"
 import LoginPage from "./Pages/LoginPage"
 import SignupPage from "./Pages/SignupPage"
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import UserProtectedWrapper from "./Pages/UserProtectedWrapper"
+
 
 function App() {
   return (
 
     <>
       <Routes>
-        {/* <Route path='/' element= {<Home/>} /> */}
+        <Route path='/' element= {<Navigate to="/login" replace />} />
         <Route path='/login' element= {<LoginPage/>}/>
         <Route path='/register' element= {<SignupPage/>}/>
         <Route path='/chat' element={
