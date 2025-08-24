9ffe0092918f45f8d2c0e017436db903624ff53f
Ritesh Sharma <riteshrsharma71@gmail.com>
2025-08-24 16:35:25 +0530

    added login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from "./Pages/LoginPage"
 import SignupPage from "./Pages/SignupPage"
 import { Navigate, Route, Routes } from 'react-router-dom'
 import UserProtectedWrapper from "./Pages/UserProtectedWrapper"
+import Loading from "./Components/Loading"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element= {<Navigate to="/login" replace />} />
+
         <Route path='/login' element= {<LoginPage/>}/>
         <Route path='/register' element= {<SignupPage/>}/>
         <Route path='/chat' element={
--- Frontend/src/Components/Loading.jsx
+++ Frontend/src/Components/Loading.jsx
@@ -2,19 +2,14 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh"
-    }}>
-      <img
-        src="https://media.tenor.com/Ug6cbVA1ZsMAAAAC/loading.gif"
-        alt="Loadingggg..."
-        width="120"
-      />
-      <p style={{ fontSize: "18px", marginTop: "10px" }}>Loading...</p>
+    <div className="fixed inset-0 flex justify-center items-center bg-black/50 backdrop-blur-sm z-50">
+      <div className="w-32 h-32 rounded-full overflow-hidden flex justify-center items-center bg-white/20 border-4 border-purple-400 animate-pulse">
+        <img
+          src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3NqbDNtbzNsandudjAzYmwzeGdzZHE2YTV6d3Q4Nzh0N2l3aXI1dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6a4udhSXVdp20zVDIz/giphy.gif"
+          alt="Loading..."
+          className="w-full h-full object-cover"
+        />
+      </div>
     </div>
   );
 };
